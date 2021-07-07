6a7b7d9a7ed2e3959e2d8298ee095cce5443bb04
Pranto Mollick <prantocse01@gmail.com>
2021-07-08 02:04:37 +0600

    project switch method

--- practice-oop-01-starting-setup/assets/scripts/app.js
+++ practice-oop-01-starting-setup/assets/scripts/app.js
@@ -5,20 +5,45 @@ class Tooltip {
 class ProjectItem {
     constructor(id) {
         this.id = id;
+        connectMoreInfoButton();
+        this.connectSwitchButton();
+    }
+
+    connectMoreInfoButton() {
+
+    }
+
+    connectSwitchButton() {
+        const projectItemElement = document.getElementById(this.id);
+        const switchBtn = projectItemElement.querySelector("button:last-of-type");
+        switchBtn.addEventListener('click', )
     }
 }
 
 class ProjectList {
     projects = [];
-
+    
 
     constructor(type) {
         const prjItems = document.querySelectorAll(`#${type}-projects li`);
-        console.log(prjItems)
         for (const prjItem of prjItems) {
             this.projects.push(new ProjectItem(prjItem.id));
         }
+        console.log(this.projects)
     }
+
+    addProject() {
+
+    }
+
+    switchProject(projectId) {
+        // const projectIndex = this.projects.findIndex(p => p.id === projectId);
+        // this.projects.splice(projectIndex, 1);
+        this.addProject();
+        this.projects = this.projects.filter(p => p.id != projectId)
+
+    }
+
 }
 
 
